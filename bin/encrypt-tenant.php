2d14f6c3a5ef92a03700588b250a4b0fe08a152a
Reinaldo Matheus <reinalddo@gmail.com>
2026-01-02 18:20:29 -0400

    Base 64

---
+++ bin/encrypt-tenant.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+use App\Application;
+use App\Tenants\CredentialCipher;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$config = require __DIR__ . '/../config/app.example.php';
+
+$app = new Application($config);
+$cipher = new CredentialCipher($app->cipher());
+
+$stdin = trim(stream_get_contents(STDIN));
+
+if ($stdin === '') {
+    fwrite(STDERR, "Proporciona el valor a cifrar por STDIN.\nEjemplo: echo valor | php bin/encrypt-tenant.php\n");
+    exit(1);
+}
+
+try {
+    $encrypted = $cipher->encrypt($stdin);
+} catch (Throwable $e) {
+    fwrite(STDERR, 'No se pudo cifrar: ' . $e->getMessage() . PHP_EOL);
+    exit(1);
+}
+
+echo $encrypted, PHP_EOL;
---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => 'Panel Master',
+    'debug' => false,
+    'timezone' => 'America/Mexico_City',
+    'encryption_key_path' => __DIR__ . '/keys/master.key',
+    'encryption_key_env' => 'PANEL_MASTER_KEY',
+];
