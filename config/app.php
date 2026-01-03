e4ff36795f9f4178871c57b6fbda8f8052af43cc
Reinaldo Matheus <reinalddo@gmail.com>
2026-01-03 18:40:36 -0400

    Credenciales App

--- config/app.php
+++ config/app.php
@@ -6,4 +6,12 @@ return [
     'timezone' => 'America/Mexico_City',
     'encryption_key_path' => __DIR__ . '/keys/master.key',
     'encryption_key_env' => 'PANEL_MASTER_KEY',
+    'db' => [
+        'host' => 'srv1999.hostg.io',
+        'port' => 3306,
+        'name' => 'u680460687_panel_master',
+        'user' => 'u680460687_admin',
+        'password' => '#+qjO0jPrEv2',
+        'charset' => 'utf8mb4',
+    ],
 ];
